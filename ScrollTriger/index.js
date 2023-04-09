033004f238e7a225efafdb6e117d4ff038ce3f87
Fiqry choerudin <fiqrychoerudin48@gmail.com>
2023-04-09 20:56:13 +0700

    feat: add starter code & scroll trigger

---
+++ ScrollTriger/index.js
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// register plugin
+gsap.registerPlugin(ScrollTrigger);
--- Timeline/index.js
+++ Timeline/index.js
@@ -3,17 +3,7 @@
 let timeline = gsap.timeline({
   repeat: 2,
   repeatDelay: 1,
-  paused: true,
+  yoyo: true,
 });
 
 timeline.to('.green', { x: 100, duration: 2 });
-// timeline.to('.purple', { x: 100, duration: 1 });
-// timeline.to('.blue', { x: 100, y: -100, duration: 1 });
-
-document
-  .querySelector('#play')
-  .addEventListener('click', () => timeline.resume());
-
-document
-  .querySelector('#pause')
-  .addEventListener('click', () => timeline.pause());
